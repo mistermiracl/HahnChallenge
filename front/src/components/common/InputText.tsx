dec47bb78847dd70118629376538414d696aa57e
he <he@sth.com>
2022-11-27 01:23:56 -0500

    wip: added value and inputRef attrs to InputText, added vertical padding

--- front/src/components/common/InputText.tsx
+++ front/src/components/common/InputText.tsx
@@ -7,20 +7,24 @@ interface InputText extends StyledProps {
     name?: string;
     label?: string;
     placeholder?: string;
+    value?: string;
+    inputRef?: React.RefObject<HTMLInputElement>;
     onInput?: (text: string) => void;
 }
 
-const InputText: FC<InputText> = ({ id, name, label, placeholder, onInput, className }) => {
+const InputText: FC<InputText> = ({ id, name, label, placeholder, value, inputRef, onInput, className }) => {
     return (
         <div className={className}>
             <div className="flex flex-col w-full h-full">
                 {label && <label htmlFor={id}>{label}</label>}
-                <input className="flex-1 w-full px-2 border-gray-100 border-2 rounded-r-sm transition-all focus:border-gray-300"
+                <input className="flex-1 w-full px-2 py-1 border-gray-100 border-2 rounded-r-sm transition-all focus:border-gray-300"
                     id={id}
                     name={name}
                     type="text" 
                     autoComplete="off" 
                     placeholder={placeholder}
+                    value={value}
+                    ref={inputRef}
                     onInput={e => onInput ? onInput(e.currentTarget.value) : undefined}
                 />
             </div>
