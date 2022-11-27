5556792660a03b85386146e4facd325ed7d428fe
he <he@sth.com>
2022-11-27 00:56:22 -0500

    wip: applying style props to InputText, making the input take as much space as possible

--- front/src/components/common/InputText.tsx
+++ front/src/components/common/InputText.tsx
@@ -10,18 +10,20 @@ interface InputText extends StyledProps {
     onInput?: (text: string) => void;
 }
 
-const InputText: FC<InputText> = ({ id, name, label, placeholder, onInput }) => {
+const InputText: FC<InputText> = ({ id, name, label, placeholder, onInput, className }) => {
     return (
-        <div>
-            {label && <label htmlFor={id}>{label}</label>}
-            <input className="flex-1 px-2 border-gray-100 border-2 rounded-r-sm transition-all focus:border-gray-300"
-                id={id}
-                name={name}
-                type="text" 
-                autoComplete="off" 
-                placeholder={placeholder}
-                onInput={e => onInput ? onInput(e.currentTarget.value) : undefined}
-            />
+        <div className={className}>
+            <div className="flex flex-col w-full h-full">
+                {label && <label htmlFor={id}>{label}</label>}
+                <input className="flex-1 w-full px-2 border-gray-100 border-2 rounded-r-sm transition-all focus:border-gray-300"
+                    id={id}
+                    name={name}
+                    type="text" 
+                    autoComplete="off" 
+                    placeholder={placeholder}
+                    onInput={e => onInput ? onInput(e.currentTarget.value) : undefined}
+                />
+            </div>
         </div>
     );
 };
