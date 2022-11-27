d8461476f0c021fbecb65e42db1656e3bd02a6f2
he <he@sth.com>
2022-11-27 00:56:46 -0500

    wip: Search component using InputText

--- front/src/components/Search.tsx
+++ front/src/components/Search.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import InputText from "./common/InputText";
 
 interface SearchProps {
     placeholder?: string
@@ -22,12 +23,7 @@ const Search: FC<SearchProps> = ({ placeholder, onSearch }) => {
             <span className="py-2 px-4 rounded-l-sm bg-gray-100">
                 <MagnifyingGlassIcon className="w-5 h-5"></MagnifyingGlassIcon>
             </span>
-            <input className="flex-1 px-2 border-gray-100 border-2 rounded-r-sm transition-all focus:border-gray-300" 
-                type="text" 
-                autoComplete="off" 
-                placeholder={placeholder ?? 'Search'}
-                onInput={e => onInput(e.currentTarget.value)}
-            />
+            <InputText className="flex-1" placeholder={placeholder} onInput={onInput}></InputText>
         </form>
     );
 };
