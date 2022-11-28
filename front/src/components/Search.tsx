b26930c5cb8ac10c8fb978f4c5bf187da1baaca6
he <he@sth.com>
2022-11-28 15:25:36 -0500

    refactor: Search component

--- front/src/components/Search.tsx
+++ front/src/components/Search.tsx
@@ -7,25 +7,13 @@ interface SearchProps {
     onSearch: (query: string) => void;
 }
 
-const Search: FC<SearchProps> = ({ placeholder, onSearch }) => {
-    const onInput = (query: string) => {
-        onSearch(query);
-    };
-
-    // const onSubmit = (e: React.FormEvent) => {
-    //     e.preventDefault();
-    //     onSearch(searchQuery);
-    // };
-
-    return (
-        // onSubmit={onSubmit}
-        <form className="flex">
-            <span className="py-2 px-4 rounded-l-sm bg-gray-100">
-                <MagnifyingGlassIcon className="w-5 h-5"></MagnifyingGlassIcon>
-            </span>
-            <InputText className="flex-1" placeholder={placeholder} onInput={onInput}></InputText>
-        </form>
-    );
-};
+const Search: FC<SearchProps> = ({ placeholder, onSearch }) => (
+    <form className="flex">
+        <span className="py-2 px-4 rounded-l-sm bg-gray-100">
+            <MagnifyingGlassIcon className="w-5 h-5"></MagnifyingGlassIcon>
+        </span>
+        <InputText className="flex-1" placeholder={placeholder} onInput={val => onSearch(val as string)}></InputText>
+    </form>
+);
 
 export default Search;
