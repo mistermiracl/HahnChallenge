635a4e77001475a6ee8890b92cc706651699ad7d
he <he@sth.com>
2022-11-30 15:14:32 -0500

    fix: InputNumber datatype

--- front/src/components/common/InputNumber.tsx
+++ front/src/components/common/InputNumber.tsx
@@ -9,27 +9,35 @@ interface InputNumberProps extends InputProps<number>, StyledProps {
     value?: number;
 }
 
-const InputNumber: FC<InputNumberProps> = ({ id, name, label, placeholder, value, min, max, disabled, required, inputRef, onChange, onInput, className }) => (
-    <div className={className}>
-        <div className="flex flex-col w-full h-full">
-            {label && <label htmlFor={id}>{label}</label>}
-            <input className="flex-1 w-full px-2 py-1 border-gray-100 border-2 rounded-r-sm transition-all focus:border-gray-300"
-                id={id}
-                name={name}
-                type="number" 
-                autoComplete="off" 
-                placeholder={placeholder}
-                min={min}
-                max={max}
-                value={value}
-                disabled={disabled}
-                required={required}
-                ref={inputRef}
-                onChange={onChange ? e => onChange(parseInt(e.currentTarget.value)) : undefined}
-                onInput={onInput ? e => onInput(parseInt(e.currentTarget.value)) : undefined}
-            />
+const InputNumber: FC<InputNumberProps> = ({ id, name, label, placeholder, value, min, max, disabled, required, inputRef, onChange, onInput, className }) => {
+
+    const getNumberValue = (val: string) => {
+        const num = parseInt(val);
+        return Number.isNaN(num) ? 0 : num;
+    };
+
+    return (
+        <div className={className}>
+            <div className="flex flex-col w-full h-full">
+                {label && <label htmlFor={id}>{label}</label>}
+                <input className="flex-1 w-full px-2 py-1 border-gray-100 border-2 rounded-r-sm transition-all focus:border-gray-300"
+                    id={id}
+                    name={name}
+                    type="number" 
+                    autoComplete="off" 
+                    placeholder={placeholder}
+                    min={min}
+                    max={max}
+                    value={value}
+                    disabled={disabled}
+                    required={required}
+                    ref={inputRef}
+                    onChange={onChange ? e => onChange(getNumberValue(e.currentTarget.value)) : undefined}
+                    onInput={onInput ? e => onInput(getNumberValue(e.currentTarget.value)) : undefined}
+                />
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default InputNumber;
