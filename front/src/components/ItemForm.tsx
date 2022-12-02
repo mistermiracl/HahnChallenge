805f96f41402e63f79690c8b101a9a367ae446fb
he <he@sth.com>
2022-12-02 16:11:08 -0500

    feat: reduced state management on ItemForm component, decoupled default values logic

--- front/src/components/ItemForm.tsx
+++ front/src/components/ItemForm.tsx
@@ -13,96 +13,100 @@ import { createItem, updateItem } from "../services/itemsService";
 
 interface ItemFormProps extends StyledProps {
     item?: Item;
-    setItem?: (item: Item) => void;
+    setItem?: (item: Item | undefined) => void;
     onSent: () => void;
 }
 
 const ItemForm: FC<ItemFormProps> = ({ item, setItem, onSent, className, style }) => {
+    const defaultFormItem: Partial<Item> = {
+        name: '',
+        quantity: 0,
+        countryId: -1,
+        colorId: -1,
+        active: false
+    };
+    
     const [countries, setCountries] = useState<Country[]>([]);
     const [colors, setColors] = useState<Color[]>([]);
+    const [formItem, setFormItem] = useState<Partial<Item>>(defaultFormItem);
     const [formDisabled, setFormDisabled] = useState(true);
 
-    const [itemName, setItemName] = useState<string>();
-    const [itemQty, setItemQty] = useState<number>();
-    const [itemCountryId, setItemCountryId] = useState<number>();
-    const [itemColorId, setItemColorId] = useState<number>();
-    const [itemActive, setItemActive] = useState<boolean>();
-
-    const [currentItem, setCurrentItem] = useState<Item>();
-
-    useEffect(() => {
-        // TODO: reset state when item changes and is present
-        if(item) {
-            setCurrentItem(item);
-            clear();
-        }
-    }, [item]);
+    // TODO: the formItem should only hold the default values if there is no item, otherwise it should all be undefined and render directly from the item
 
     useEffect(() => {
         (async () => {
             const [fetchedCountries, fetchedColors] = await Promise.all([getCountries(), getColors()]);
             setCountries(fetchedCountries);
             setColors(fetchedColors);
-            setItemCountryId(fetchedCountries?.[0].id);
-            setItemColorId(fetchedColors?.[0].id);
+            if(!item) {
+                setDefaultValues(fetchedCountries, fetchedColors);
+            }
             setFormDisabled(false);
         })();
     }, []);
 
-    // const setDefaultValues = (countries: Country[], colors: Color[]) => {
-    //     if(!item) {
-    //         setItemName('');
-    //         setItemQty(0);
-    //         setItemCountryId(countries?.[0].id);
-    //         setItemColorId(colors?.[0].id);
-    //         setItemActive(false);
-    //     }
-    // };
+    useEffect(() => {
+        if(item) {
+            clear(true);
+            setFormItemPartial({
+                id: item.id
+            });
+        }
+    }, [item]);
 
-    const clear = () => {
-        setItemName(undefined);
-        setItemQty(undefined);
-        setItemCountryId(undefined);
-        setItemColorId(undefined);
-        setItemActive(undefined);
+    const setFormItemPartial = (itemFields: Partial<Item>) => {
+        setFormItem(prevItem => (
+            { ...prevItem, ...itemFields }
+        ));
     };
 
-    // const reset = (resetCountries?: Country[], resetColors?: Color[]) => {
-    //     clear();
-    //     setDefaultValues(resetCountries ?? countries, resetColors ?? colors);
-    // }
+    const setDefaultValues = (countries: Country[], colors: Color[]) => {
+        setFormItemPartial(({
+            ...defaultFormItem,
+            countryId: countries?.[0]?.id ?? -1,
+            colorId: colors?.[0]?.id ?? -1
+        }));
+    };
+
+    const clear = (unset?: boolean) => {
+        if(unset) {
+            setFormItem({});
+        } else {
+            setDefaultValues(countries, colors);
+        }
+    };
+
+    const reset = () => {
+        if(setItem) setItem(undefined);
+        clear();
+    };
 
     const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         // TODO: insert data using api
         if(item) {
-            const updatedItem = new Item(itemName!, itemQty!, itemCountryId!, itemColorId!, itemActive!, item.id);
-            await updateItem(updatedItem);
+            await updateItem(formItem);
         } else {
-            const newItem = new Item(itemName!, itemQty!, itemCountryId!, itemColorId!, itemActive!);
-            await createItem(newItem);
+            await createItem(formItem as Item);
         }
-        clear();
-        console.log({ id: currentItem?.id, itemName, itemQty, itemCountryId, itemColorId, itemActive });
+        reset();
+        console.log(formItem);
         onSent();
     };
 
     const onCancel = () => {
-        if(item) setCurrentItem(undefined);
-        clear();
-        setItemCountryId(countries?.[0].id);
-        setItemColorId(colors?.[0].id);
+        console.log(item);
+        reset();
     };
 
-    // TODO: border bottom none, border radius bottom none, padding bottom none?
     return (
         <div className={className} style={style}>
             <form onSubmit={onSubmit}>
                 <fieldset className="px-4 py-6 border-gray-100 border-x-2 border-t-2 rounded-t-sm">
-                    <legend className="text-lg">{ item ? 'Edit' : 'New' } Item</legend>
+                    <legend className="text-xl font-semibold">{ item ? 'Edit Item' : 'New Item' }</legend>
                     <div className="flex items-center gap-2 mb-3">
                         <InputText className="w-14" inputClassName="text-center" label="ID" disabled horizontal 
-                            value={currentItem?.id ?? '-'} />
+                            value={item?.id ?? '-'} />
                         <div className="flex gap-1 ml-auto">
                             <label>Active</label>
                             <input className="appearance-none w-7 h-7 p-1 border-gray-100 border-2 rounded-sm
@@ -110,22 +114,22 @@ const ItemForm: FC<ItemFormProps> = ({ item, setItem, onSent, className, style }
                                 before:block before:w-full before:h-full before:rounded-sm before:transition-all
                                 checked:before:bg-black"
                                 type="checkbox"
-                                checked={itemActive ?? currentItem?.active ?? false}
-                                onChange={e => setItemActive(e.currentTarget.checked)} />
+                                checked={formItem.active ?? item?.active}
+                                onChange={e => setFormItemPartial({ active: e.currentTarget.checked })} />
                         </div>
                     </div>
                     <div>
                         <InputText className="mb-3" label="Name"
-                            value={itemName ?? currentItem?.name ?? ''}
-                            onChange={val => setItemName(val as string)} minLength={3} required />
+                            value={formItem.name ?? item?.name}
+                            onChange={val => setFormItemPartial({ name: val as string })} minLength={3} required />
                     </div>
                     <div className="flex gap-3 mb-3">
                         <InputNumber className="flex-1" label="Quantity" min={0}
-                            value={itemQty ?? currentItem?.quantity ?? 0}
-                            onChange={val => setItemQty(val)} required />
+                            value={formItem.quantity ?? item?.quantity}
+                            onChange={val => setFormItemPartial({ quantity: val })} required />
                         <Select className="flex-1" label="Country"
-                            value={itemCountryId ?? currentItem?.countryId}
-                            onChange={val => setItemCountryId(parseInt(val))}>
+                            value={formItem.countryId ?? item?.countryId}
+                            onChange={val => setFormItemPartial({ countryId: parseInt(val) })}>
                             {countries.length ? countries.map(country => (
                                 <option key={country.id} value={country.id}>{country.name}</option>
                             )) : (
@@ -140,8 +144,8 @@ const ItemForm: FC<ItemFormProps> = ({ item, setItem, onSent, className, style }
                                 <div key={color.id} className="flex gap-1">
                                     <span className="inline-block w-5 h-5 rounded-full" style={{ backgroundColor: color.hex }}></span>
                                     <input className="w-4 accent-black" type="radio" name="color" value={color.id}
-                                        checked={color.id === itemColorId || color.id === currentItem?.colorId}
-                                        onChange={e => setItemColorId(parseInt(e.currentTarget.value))}/>
+                                        checked={color.id === (formItem.colorId ?? item?.colorId)}
+                                        onChange={e => setFormItemPartial({ colorId: parseInt(e.currentTarget.value) })}/>
                                 </div>
                             )) : (
                                 <>
