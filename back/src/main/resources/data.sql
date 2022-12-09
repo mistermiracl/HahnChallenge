94340ca6548a3110ef5b596001faf0b75cdc3cd2
he <he@sth.com>
2022-12-09 17:59:47 -0500

    dist: database seeding

---
+++ back/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO country (name)
+VALUES ('Peru'), ('Bolivia'), ('Colombia'), ('Brazil'), ('Ecuador');
+
+INSERT INTO color (name, hex)
+VALUES ('Red', '#ff0000'), ('Yellow', '#ffeb3b'), ('Blue', '#3f51b5'), ('Green', '#4caf50');
