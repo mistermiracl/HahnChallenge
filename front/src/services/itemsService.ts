25c00c66793eed7fe421ada277ee7510bf1574b4
he <he@sth.com>
2022-12-09 19:33:00 -0500

    feat: removed dummy operations, added api calls for all crud operations on itemsService

--- front/src/services/itemsService.ts
+++ front/src/services/itemsService.ts
@@ -1,32 +1,33 @@
-import Color from "../models/color";
-import Country from "../models/country";
 import Item from "../models/item"
 
-const itemsUrl = import.meta.env.BASE_URL 
-
-const dummyItems: Item[] = [
-    new Item('Pokeball', 2, 1, 1, true, 10, new Country(1, 'Peru'), new Color(1, 'Red', '#ff0000')),
-    new Item('Masterball', 1, 2, 2, false, 11, new Country(2, 'Bolivia'), new Color(2, 'Yellow', '#ffeb3b'))
-];
+const itemsUrl = import.meta.env.VITE_API_URL + '/items'; 
 
 export function getItems(): Promise<Item[]> {
-    return Promise.resolve([...dummyItems]);
+    return fetch(itemsUrl).then(res => res.json());
 }
 
 export function createItem(item: Item) {
-    item.id = dummyItems[dummyItems.length - 1].id! + 1;
-    dummyItems.push(item);
-    return Promise.resolve({ status: 'ok' });
+    return fetch(itemsUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    }).then(res => res.json());
 }
 
-export function updateItem(item: Partial<Item>) {
-    const itemIndex = dummyItems.findIndex(dummy => dummy.id === item.id);
-    dummyItems[itemIndex] = { ...dummyItems[itemIndex], ...item };
-    return Promise.resolve({ status: 'ok' });
+export function updateItem(id: number, item: Partial<Item>) {
+    return fetch(itemsUrl + '/' + id, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    }).then(res => res.json());
 }
 
 export function deleteItem(itemId: number) {
-    const itemIndex = dummyItems.findIndex(dummy => dummy.id === itemId);
-    dummyItems.splice(itemIndex, 1);
-    return Promise.resolve({ status: 'ok' });
+    return fetch(itemsUrl + '/' + itemId, {
+        method: 'DELETE',
+    }).then(res => res.json());
 }
